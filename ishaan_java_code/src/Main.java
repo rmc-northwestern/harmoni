8f87231bc48e9ba91411318cf73491d151afdf45
Ishaan Madan <ishaanmadan2020@u.northwestern.edu>
2019-03-11 15:00:28 -0500

    ishaan code

---
+++ ishaan_java_code/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        midiPlayer x = new midiPlayer();
+    }
+
+}
