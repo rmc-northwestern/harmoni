c89de4aecaea0c1d193ed3c0ebac2b54ce586d88
Ryan McHenry <ryanmchenry2@gmail.com>
2019-03-13 13:26:06 -0500

    updating server to take file path input

--- server/files/test.java
+++ server/files/test.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class test {
 
-	private static final String FILENAME = "files/test.txt";
+	private static final String FILENAME = "files/result.txt";
 
 	public static void main(String[] args) {
 
@@ -13,13 +13,13 @@ public class test {
 
 		try {
 
-			String content = "let's test this here boi";
+			String content = args[0];
 
 			fw = new FileWriter(FILENAME);
 			bw = new BufferedWriter(fw);
 			bw.write(content);
 
-			System.out.println("Done");
+			System.out.println(args[0] + " is written to file " + FILENAME);
 
 		} catch (IOException e) {
 
