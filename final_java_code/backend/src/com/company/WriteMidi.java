2e1c54a08d3fe831ea5ed8608d6321a098201231
Ryan McHenry <ryanmchenry2@gmail.com>
2019-03-20 20:50:01 -0500

    simplified loop

--- final_java_code/backend/src/com/company/WriteMidi.java
+++ final_java_code/backend/src/com/company/WriteMidi.java
@@ -12,33 +12,32 @@ public class WriteMidi {
 
             Track[] a = sequence.getTracks();
             Track track1 = a[0];
-            Track [] t_arr = new Track[3];
-            Track one = a[0];
-            Track three = a[0];
-            Track five = a[0];
-            t_arr[0] = one;
-            t_arr[1] = three;
-            t_arr[2] = five;
+            // Track [] t_arr = new Track[3];
+            // Track one = a[0];
+            // Track three = a[0];
+            // Track five = a[0];
+            // t_arr[0] = one;
+            // t_arr[1] = three;
+            // t_arr[2] = five;
 
-            for (int i =0; i<t_arr.length;i++) {
-                for (int k = 0; k < t_arr[i].size(); k++) {
-                    MidiEvent event = track1.get(k);
-                    MidiMessage msg = event.getMessage();
-                    byte[] data = msg.getMessage();
-                    String s1 = String.format("%8s", Integer.toBinaryString(data[0] & 0xFF)).replace(' ', '0');
-                    String s2 = "1001";
-                    String s3 = "1000";
-                    if (s1.startsWith(s2)) {
-                        data[1] = 59;
-                        System.out.println(k + " is the index of k. The note is: " + +data[1] + "and the velocity is: " + data[2]);
+              for (int i = 0; i<track1.size(); i++){
+                  MidiEvent event = track1.get(i);
+                  MidiMessage msg = event.getMessage();
+                  byte[] data = msg.getMessage();
+                  String s1 = String.format("%8s", Integer.toBinaryString(data[0] & 0xFF)).replace(' ', '0');
+                  String s2 = "1001";
+                  String s3 = "1000";
+                  if (s1.startsWith(s2)) {
+                      data[1] = 59;
+                      System.out.println(k + " is the index of k. The note is: " + +data[1] + "and the velocity is: " + data[2]);
 
-                    }
-                    if (s1.startsWith(s3)) {
-                        data[1] = 59;
-                    }
-                    sequence.createTrack().add(event);
-                }
-            }
+                  }
+                  if (s1.startsWith(s3)) {
+                      data[1] = 59;
+                  }
+                  sequence.track1.add(event);
+              }
+            // }
 
 
                     File f = new File("midifiler.mid");
